f8892f94874445ae6bcc6ceb2941c0833b292b97
JuanitaCeci <juanitaceci0624@gmail.com>
2022-09-27 20:22:26 -0400

    class04

---
+++ src/Class03B14/class04.java
@@ -0,0 +1,13 @@
+package Class03B14;
+
+public class class04 {
+
+	public static void main(String[] args) {
+
+			String word="This is a new class";
+			
+			System.out.println("Hello this is a new class");
+
+	}
+
+}
